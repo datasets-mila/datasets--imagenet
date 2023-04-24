fc764d409b8ed421481a9bb945bd1e32de75ae53
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2023-04-24 15:34:24 -0400

    Update scripts/preprocess_torchvision.sh

--- scripts/preprocess_torchvision.sh
+++ scripts/preprocess_torchvision.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
+source scripts/utils.sh echo -n
 
-python scripts/preprocess_torchvision.py
+# Saner programming env: these switches turn some bugs into errors
+set -o errexit -o pipefail
 
-rm files_count.stats
-for dir in train/* val/*
+# This script is meant to be used with the command 'datalad run'
+
+_SNAME=$(basename "$0")
+
+mkdir -p logs/
+
+# Transform dataset
+python3 -m pip install -r scripts/requirements_torchvision.txt || \
+	exit_on_error_code "Failed to install requirements: pip install"
+
+python3 scripts/preprocess_torchvision.py \
+	1>>logs/${_SNAME}.out_$$ 2>>logs/${_SNAME}.err_$$
+
+# Compile stats
+# this is to have an idea of the internal state of the dataset
+for d in train/ val/
 do
-	echo $(find $dir -type f | wc -l; echo $dir) >> files_count.stats
-done
+	printf "%s\n" "${d}"
+done | sort -u | ./scripts/stats.sh
 
-du -s train/* val/* > disk_usage.stats
+git-annex add --fast -c annex.largefiles=nothing *.stats
 
 rm ILSVRC2012_*
